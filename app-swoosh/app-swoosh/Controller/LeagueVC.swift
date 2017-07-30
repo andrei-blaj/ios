2c53faf9ae111ac8b3a4c6e1bd6210495d86906f
Andrei Blaj <andreiblaj17@gmail.com>
2017-07-30 15:05:29 +0300

    Added segues, refactoring, passing data between view controller

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,61 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Andrei-Sorin Blaj on 30/07/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        player = Player()
+        
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    
+    // onNextTapped performs the segue for going from the current view controller to the next
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // preapareForSegue is always called before viewDidLoad ( on the destination view controller )
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
+
+}
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Andrei-Sorin Blaj on 30/07/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
