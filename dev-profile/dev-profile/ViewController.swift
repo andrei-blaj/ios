45f28f82d43b27845940b6b171cb9f3d474b9425
Andrei Blaj <andreiblaj17@gmail.com>
2017-07-30 17:58:44 +0300

    Added new project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Andrei-Sorin Blaj on 30/07/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
