aed498298ad334b987eb9ddefb426d22c6dd4799
Andrei Blaj <andreiblaj17@gmail.com>
2017-08-09 18:11:27 +0300

    Added the login screen

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Andrei-Sorin Blaj on 09/08/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Andrei-Sorin Blaj on 09/08/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
