56efd29681bd657fe70208e5a3a6aea2fdf49e2f
Andrei Blaj <andreiblaj17@gmail.com>
2017-08-18 17:19:56 +0300

    Initial commit for PixelCity

---
+++ PixelCity/PixelCity/Controller/MapVC.swift
@@ -0,0 +1,19 @@
+//
+//  MapVC.swift
+//  PixelCity
+//
+//  Created by Andrei-Sorin Blaj on 18/08/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
