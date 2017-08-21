14aaaebc1650cdde2f2aa3ed08036ea1e388da8d
Andrei Blaj <andreiblaj17@gmail.com>
2017-08-22 01:57:00 +0300

    Added double tap to dismiss PopVC

---
+++ PixelCity/PixelCity/Controller/PopVC.swift
@@ -0,0 +1,42 @@
+//
+//  PopVC.swift
+//  PixelCity
+//
+//  Created by Andrei-Sorin Blaj on 22/08/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import UIKit
+
+class PopVC: UIViewController, UIGestureRecognizerDelegate {
+
+    // Outlets
+    @IBOutlet weak var popImageView: UIImageView!
+    
+    // Variables
+    var passedImage: UIImage!
+    
+    func initData(forImage image: UIImage) {
+        self.passedImage = image
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        popImageView.image = passedImage
+        addDoubleTap()
+    }
+
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
+        
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        
+        view.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func screenWasDoubleTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
