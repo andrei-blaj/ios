5af025e75aa7ccdfde2ff6b347d5d72e558655cd
Andrei Blaj <andreiblaj17@gmail.com>
2017-08-28 23:26:39 +0300

    Updated GoalCell

---
+++ GoalPost/GoalPost/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by Andrei-Sorin Blaj on 27/08/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
