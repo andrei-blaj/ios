ede1a6d0bdd4978d72c1ce95b47018c9dd353b77
Andrei Blaj <andreiblaj17@gmail.com>
2017-08-30 15:43:02 +0300

    Fetching from Core Data

--- GoalPost/GoalPost/Controller/FinishGoalVC.swift
+++ GoalPost/GoalPost/Controller/FinishGoalVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
 
@@ -39,8 +40,35 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func createGoalBtnPressed(_ sender: Any) {
-        
         // Pass data into Core Data Goal Model
+        
+        if pointsTextField.text != "" {
+            self.save(completion: { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            })
+        }
+        
+    }
+    
+    func save(completion: (_ finished: Bool) -> ()) {
+        
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        
+        goal.goalType = goalType.rawValue
+        goal.goalDescription = goalDescription
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+            try managedContext.save()
+            completion(true)
+        } catch {
+            debugPrint("Could not save: \(error.localizedDescription)")
+            completion(false)
+        }
         
     }
     
--- GoalPost/GoalPost/Controller/GoalsVC.swift
+++ GoalPost/GoalPost/Controller/GoalsVC.swift
@@ -7,16 +7,40 @@
 //
 
 import UIKit
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
 class GoalsVC: UIViewController {
 
+    // Outlets
     @IBOutlet weak var tableView: UITableView!
+    
+    // Variables
+    var goals: [Goal] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.fetch { (success) in
+            if success {
+                if goals.count > 0 {
+                    tableView.isHidden = false
+                } else {
+                    tableView.isHidden = true
+                }
+            }
+        }
+        
+        tableView.reloadData()
         
     }
 
@@ -36,18 +60,68 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return goals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
         
-        cell.configureCell(description: "Eat salad twice a week.", type: .shortTerm, goalProgressAmount: 2)
+        let goal = goals[indexPath.row]
+        
+        cell.configureCell(goal: goal)
         
         return cell
         
     }
     
 }
+
+extension GoalsVC {
+    
+    func fetch(completion: (_ success: Bool) -> ()) {
+        
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        let fetchRequest = NSFetchRequest<Goal>(entityName: "Goal")
+        
+        do {
+            goals = try managedContext.fetch(fetchRequest)
+            completion(true)
+        } catch {
+            debugPrint("Could not fetch: \(error.localizedDescription)")
+            completion(false)
+        }
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
