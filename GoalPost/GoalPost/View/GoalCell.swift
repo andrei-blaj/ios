aff8dd46a72ace472c9329e1be0a25ab7f7331f1
Andrei Blaj <andreiblaj17@gmail.com>
2017-08-30 21:16:31 +0300

    Setting goal progress for UITableViewCell

--- GoalPost/GoalPost/View/GoalCell.swift
+++ GoalPost/GoalPost/View/GoalCell.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class GoalCell: UITableViewCell {
 
+    // Outlets
     @IBOutlet weak var goalDescriptionLbl: UILabel!
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var goalProgressLbl: UILabel!
+    @IBOutlet weak var completionView: UIView!
+    
     
     func configureCell(goal: Goal) {
         
@@ -20,6 +23,12 @@ class GoalCell: UITableViewCell {
         self.goalTypeLbl.text = goal.goalType
         self.goalProgressLbl.text = String(describing: goal.goalProgress)
     
+        if goal.goalProgress == goal.goalCompletionValue {
+            self.completionView.isHidden = false
+        } else {
+            self.completionView.isHidden = true
+        }
+        
     }
 
 }
