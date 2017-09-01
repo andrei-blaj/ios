6905a0c0c06da76ad665dff4e99fa4a268ffcf3a
Andrei Blaj <andreiblaj17@gmail.com>
2017-09-01 16:12:06 +0300

    Buidling MeVC and adding it to the UITabBarController

---
+++ breakPoint/breakPoint/Controller/MeVC.swift
@@ -0,0 +1,27 @@
+//
+//  MeVC.swift
+//  breakPoint
+//
+//  Created by Andrei-Sorin Blaj on 01/09/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func signOutBtnPressed(_ sender: Any) {
+        // Sign out code
+    }
+    
+}
