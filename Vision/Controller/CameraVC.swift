be0655723c38acbb0614906a0665287f226069f9
Andrei Blaj <andreiblaj17@gmail.com>
2017-09-06 00:34:44 +0300

    Initial commit for Vision App (Core ML)

---
+++ Vision/Controller/CameraVC.swift
@@ -0,0 +1,19 @@
+//
+//  CameraVC.swift
+//  Vision
+//
+//  Created by Andrei-Sorin Blaj on 06/09/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
