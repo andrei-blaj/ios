6dfe43c28e9a56a9c2383c3974337e0b67512957
Andrei Blaj <andreiblaj17@gmail.com>
2017-09-06 12:45:44 +0300

    AVFoundation & AVCaptureSession to use the camera

--- Vision/Controller/CameraVC.swift
+++ Vision/Controller/CameraVC.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
 
+    // Variables
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
     // Outlets
     @IBOutlet weak var roundedView: RoundedShadowView!
     @IBOutlet weak var captureImageView: RoundedShadowImageView!
@@ -20,6 +26,46 @@ class CameraVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            
+            if captureSession.canAddInput(input) == true {
+                captureSession.addInput(input)
+            }
+            
+            cameraOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddOutput(cameraOutput) == true {
+                captureSession.addOutput(cameraOutput!)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+                previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                
+                cameraView.layer.addSublayer(previewLayer!)
+                captureSession.startRunning()
+                
+            }
+            
+        } catch {
+            debugPrint(error)
+        }
+        
     }
 
 }
