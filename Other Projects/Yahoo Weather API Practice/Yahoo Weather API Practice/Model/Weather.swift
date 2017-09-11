47d178134374c6f5d7108d8a10d5b0ed093746fa
Andrei Blaj <andreiblajdev@gmail.com>
2017-09-11 17:17:21 +0300

    Added codes for weather conditions

---
+++ Other Projects/Yahoo Weather API Practice/Yahoo Weather API Practice/Model/Weather.swift
@@ -0,0 +1,48 @@
+//
+//  Weather.swift
+//  Yahoo Weather API Practice
+//
+//  Created by Andrei-Sorin Blaj on 11/09/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import Foundation
+
+class Weather {
+    
+    // Units
+    var _distance: String! // "mi"
+    var _pressure: String! // "in"
+    var _speed: String! // "mph"
+    var _temperature: String! // "F"
+    
+    // Location
+    var _city: String!
+    var _country: String!
+    var _region: String!
+
+    // Wind
+    var _windChill: String!
+    var _windDirection: String!
+    var _windSpeed: String!
+    
+    // Atmosphere
+    var _atmosphericHumidity: String!
+    var _atmosphericPressure: String!
+    var _atmosphericVisibility: String!
+
+    // Astronomy
+    var _sunrise: String!
+    var _sunset: String!
+    
+    // Condition
+    
+    
+    // Setters & Getters
+    
+    // Units
+    
+    
+    
+}
+
