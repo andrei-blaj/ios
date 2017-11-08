da26f6062edbef432a54ac08993f00fba8bce4ed
Andrei Blaj <andreiblajdev@gmail.com>
2017-11-09 00:14:30 +0200

    Created iOS version of Company Administrator

---
+++ Co. Administrator/Co. Administrator/Controller/SideMenuVC.swift
@@ -0,0 +1,19 @@
+//
+//  SideMenuVC.swift
+//  Co. Administrator
+//
+//  Created by Andrei-Sorin Blaj on 08/11/2017.
+//  Copyright © 2017 Andrei-Sorin Blaj. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+}
